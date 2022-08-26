759c2e003f29cf8b12c72535ce550aeaf2ce0b7d
Gregory Lureau <glureau@deezer.com>
2022-08-26 16:55:54 +0200

    Use JVM instead of Android when both are available.

--- mockmp-gradle-plugin/src/main/kotlin/org/kodein/mock/gradle/MocKMPGradlePlugin.kt
+++ mockmp-gradle-plugin/src/main/kotlin/org/kodein/mock/gradle/MocKMPGradlePlugin.kt
@@ -26,9 +26,12 @@ class MocKMPGradlePlugin : Plugin<Project> {
         target.extensions.add("mockmp", ext)
 
         target.afterEvaluate {
-            val kotlin = extensions.findByType<KotlinMultiplatformExtension>() ?: throw GradleException("Could not find Kotlin/Multiplatform plugin")
+            val kotlin = extensions.findByType<KotlinMultiplatformExtension>()
+                ?: throw GradleException("Could not find Kotlin/Multiplatform plugin")
 
-            val jvmTarget = kotlin.targets.first { it.preset?.name == "jvm" || it.preset?.name == "android" }
+            val jvmTarget = kotlin.targets.firstOrNull { it.preset?.name == "jvm" }
+                ?: kotlin.targets.firstOrNull { it.preset?.name == "android" }
+                ?: throw GradleException("MocKMP requires platform `jvm` or `android` to be defined")
 
             dependencies {
                 // Running KSP for JVM only
